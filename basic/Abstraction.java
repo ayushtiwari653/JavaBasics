64836ff6516e7dc0c955c873160354e95dbac25b
iam_ayushtiwari <31324791+ayushtiwari653@users.noreply.github.com>
2020-12-14 23:16:13 +0530

    third commit

--- basic/Abstraction.java
+++ basic/Abstraction.java
@@ -3,17 +3,25 @@ package basic;
 public class Abstraction {
 
 	public static void main(String[] args) {
-		BMW car=new BMW();
-		car.start();car.price=10000;
-		
-		System.out.println(car.price);
-		car.Break();
+		BMW car1=new BMW(1000);
+		audi car2=new audi(5000); 
+		car1.start();
+		car2.start();
+		System.out.println(car1.price);
+		System.out.println(car2.price);
+		car1.Break();
 	}
 
 }
 
 
 class audi extends car{
+	audi(){
+		super.price=0;
+	}
+	audi(int price){
+		super.price=price;
+	}
 
 	@Override
 	void start() {
@@ -25,6 +33,13 @@ class audi extends car{
 }
 
 class BMW extends car{
+	BMW(){
+		super.price=0;
+	}
+	BMW(int price){
+		this();
+		super.price=price;
+	}
 
 	@Override
 	void start() {
